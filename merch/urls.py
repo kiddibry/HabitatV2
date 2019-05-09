27aa11eca03e3a5aa65bfd3dcc86575b506693b1
Styrkar Blaer <stykki@hotmail.com>
2019-05-09 14:43:10 +0000

    un-ugly-fying the houses

--- merch/urls.py
+++ merch/urls.py
@@ -3,12 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('', views.index, name = "merch-index")
-=======
     path('', views.index, name = "merch-index"),
     path('bags', views.bags, name = "merch_bags"),
     path('hats', views.hats, name = "merch_hats"),
     path('t-shirts', views.tshirts, name = "merch_t-shirts"),
->>>>>>> cbb5355a71ad5f67ffb1ac3cd7f55073e6c84e8d
 ]
