28552780c1e79dface8e0f7bd9f4126976464936
kiddibry <mrkrissibry@gmail.com>
2019-05-10 12:12:25 +0000

    bætti við meira dóti í profile

--- user/forms/profile_form.py
+++ user/forms/profile_form.py
@@ -5,7 +5,10 @@ from user.models import Profile
 class ProfileForm(ModelForm):
     class Meta:
         model = Profile
-        exclude = ['id', 'user']
+        exclude = ['id', 'user', 'is_admin']
         widgets = {
+            'name': widgets.TextInput(attrs={'class': 'form-control'}),
+            'mail':  widgets.TextInput(attrs={'class': 'form-control'}),
+            'info':  widgets.TextInput(attrs={'class': 'form-control'}),
             'profile_image': widgets.TextInput(attrs={'class': 'form-control'}),
         }
