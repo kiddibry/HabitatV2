84d6d1a0ef5ab3336c739a5d8ff5d2edb4dea6b6
logisteinn <34041474+logisteinn@users.noreply.github.com>
2019-05-14 14:48:00 +0000

    Admin réttindi komið fyrir buy house

--- static/js/houses.js
+++ static/js/houses.js
@@ -7,13 +7,27 @@ $(document).ready(function(){
                 type: 'GET',
             success: function (resp) {
                 var newHtml = resp.data.map(d => {
-                    return `<div>
-                                <a href="/houses/${d.id}">    
+                    return `
+                            <div>
+                                <a href="/houses/${d.id}" > 
+                                <div class="Boxman">   
                                     <img src="${d.firstImage}" alt="image"/>
                                     <h4>${d.name}</h4>
                                     <p>${d.description}</p>
+                                 </div>
                                 </a>
-                            </div>`
+                            </div>
+
+                            <style>
+                                .Boxman {
+                                    border: 1px solid black;
+                                    margin-bottom: 1em;
+                                }
+                            
+                            </style>
+
+
+                            `
                 });
                 $('.houses').html(newHtml.join(''));
                 $('#search-box').val('');
