0e66c69271a5e600d24099e518e2e455ddc513e8
kiddibry <mrkrissibry@gmail.com>
2019-05-17 13:24:59 +0000

    hægt að adda og deleta myndum fyrir hús

--- house/urls.py
+++ house/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('update_house/<int:id>', views.update_house, name='update_house'),
     path('buy/<int:id>', views.buy_house, name='buy'),
     path('bought/<int:id>', views.bought, name='house_bought'),
-    path('images/<int:id>', views.add_images, name='add_images')
+    path('images/<int:id>', views.add_images, name='add_images'),
+    path('delete_image/<int:id>', views.remove_image, name='delete_image')
 ]
